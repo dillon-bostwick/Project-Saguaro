05bf81f50def5c698a9071edd3ab7c1e2270421b
Dillon Bostwick <dillon.bostwick@gmail.com>
2016-08-11 13:38:45 -0400

    First commit: Express template and started SOAP protocol

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+var qbws = require('qbws');
+
+qbws.run();
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+ 
+// app.use('/', routes);
+// app.use('/users', users);
+
+////////////////////////////////////
+
+app.post('/newbill', function(req, res) {
+  var foo = req.body;
+})
+
+app.post('/newinvoice', function(req, res) {
+  var invoice = req.body;
+})
+
+////////////////////////////////////
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+//module.exports = app;
