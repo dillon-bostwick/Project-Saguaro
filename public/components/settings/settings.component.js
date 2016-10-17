bf7894110166c85ad9578fb1dfd5e973d2b0420b
Dillon Bostwick <dillon.bostwick@gmail.com>
2016-10-17 15:42:12 -0400

    Started working on accordion for setting menu

--- public/components/settings/settings.component.js
+++ public/components/settings/settings.component.js
@@ -4,13 +4,30 @@ angular.
     module('settings').
     component('settings', {
         templateUrl: 'components/settings/settings.template.html',
-        controller: function SettingsController(api, $window) {
+        controller: function SettingsController(api, $window, $location) {
         	var self = this;
             window.ctrl = self;
             self.path = $window.location.hash
 
             ////////////////////////////////////////////////////////////////////
 
-            
+            // External requests:
+            self.Vendors = api.Vendor.query();
+            self.Hoods = api.Hood.query();
+            self.Expenses = api.Expense.query();
+            self.Activities = api.Activity.query();
+            self.Invoices = api.Invoice.query();
+            self.CurrentUser = api.CurrentUser.get();
+
+            ////////////////////////////////////////////////////////////////////
+
+            self.groups = [
+                {
+                  title: 'Dynamic Group Header - 1',
+                  template: 'hello.html'
+                }
+            ];
+
+
     	}
     });
