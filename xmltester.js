71ceb74ca1eb1394dbd47091c470f2734491a446
Dillon Bostwick <dillon.bostwick@gmail.com>
2016-11-04 13:18:30 -0400

    heroku 11

---
+++ xmltester.js
@@ -0,0 +1,25 @@
+var builder = require('xmlbuilder');
+
+inputXML = builder.create('QBXML', { version: '1.0'})
+                     .instruction('qbxml', 'version="13.0"')
+                     .ele('QBXLMMsgsRq', { onError: 'stopOnError' })
+                        .ele('IBillAdd', { requestID: '3' })
+                            .ele('memo')
+                                .text('2 expenses')
+                            .up()
+                            .ele('ExpenseLineAddList')
+                                .ele('AccountRef')
+                                    .ele('FullName')
+                                        .text('first expense code')
+                                    .up()
+                                .up()
+                            .up()
+                            .ele('ExpenseLineAddList')
+                                .ele('AccountRef')
+                                    .ele('FullName')
+                                        .text('second expense code')
+                                    .up()
+                                .up()
+                            .up()
+
+module.exports = inputXML.end();
